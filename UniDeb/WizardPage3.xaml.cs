f70184739699cd798f40b3ec57ce3f22d46a39e5
fazek42 <kisbalint42@gmail.com>
2015-06-28 15:30:43 +0200

    Mezok kesz, + HTML previewer

--- UniDeb/WizardPage3.xaml.cs
+++ UniDeb/WizardPage3.xaml.cs
@@ -21,11 +21,60 @@ namespace UniDeb
     public partial class WizardPage3 : Window
     {
         private String initString;
+        private String egybenString;
+        private String[] mezok = new String[10];
 
         public WizardPage3(String initString)
         {
             InitializeComponent();
-            this.initString = initString;
+            mezok[0] = initString;
+            this.initString = initString + "$";
+            egybenString = this.initString;
+
+        }
+
+        private void BtnPreview_Click(object sender, RoutedEventArgs e)
+        {
+            Strngesites();
+            TxtbxPreview.Text = this.egybenString;
+
+        }
+
+        private void Strngesites()
+        {
+            this.egybenString = this.egybenString
+                    + Txtbx3_1_1.Text + "$"
+                    + Txtbx3_1_2.Text + "$"
+                    + Txtbx3_1_3.Text + "$"
+                    + Txtbx3_1_4.Text + "$"
+                    + Txtbx3_1_5.Text + "$"
+                    + Txtbx3_1_6.Text + "$"
+                    + Txtbx3_1_7.Text + "$"
+                    + Txtbx3_1_8.Text + "$"
+                    + Txtbx3_1_9.Text + "$";
+
+            mezok[1] = Txtbx3_1_1.Text;
+            mezok[2] = Txtbx3_1_2.Text;
+            mezok[3] = Txtbx3_1_3.Text;
+            mezok[4] = Txtbx3_1_4.Text;
+            mezok[5] = Txtbx3_1_5.Text;
+            mezok[6] = Txtbx3_1_6.Text;
+            mezok[7] = Txtbx3_1_7.Text;
+            mezok[8] = Txtbx3_1_8.Text;
+            mezok[9] = Txtbx3_1_9.Text;
+        }
+
+        private void BtnHTMLPreview_Click(object sender, RoutedEventArgs e)
+        {
+            String htmlWebPage = "<head><meta http-equiv='Content-Type' content='text/html;charset=UTF-8'></head><body>";
+            foreach (String temp in mezok)
+            {
+                htmlWebPage += temp;
+            }
+            htmlWebPage += "</body>";
+
+            HTMLPreviewWindow htmlWindow = new HTMLPreviewWindow(htmlWebPage);
+            htmlWindow.Show();
         }
     }
 }
